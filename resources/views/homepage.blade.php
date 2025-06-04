575b2bc48a9853bdc4f18dd2b262bfafa54a6772
Yori Adi Atma <yoriadiatma@gmail.com>
2025-06-04 15:54:48 +0700

    Middleware and Authorization

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,7 +10,7 @@
             <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="{{ $movie->cover_image }}" class="img-fluid rounded-start" alt="...">
+                        <img src="{{ asset('storage/'.$movie->cover_image) }}" class="card-img-top" alt="{{ $movie->title }}">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,40 +3,66 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Movie DB - @yield('title', 'Homepage')</title>
+    <title>Movie DB</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   </head>
   <body>
+    {{-- Navbar  --}}
     <nav class="navbar navbar-expand-lg bg-success" data-bs-theme="dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">Movie DB</a>
+    <div class="container">
+        <a class="navbar-brand" href="/">Movie DB</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
+            @auth
             <li class="nav-item">
-              <a class="nav-link" href="#">Watchlist</a>
+                <a class="nav-link" href="/movie/create">Input Movie</a>
             </li>
-          </ul>
-          <form class="d-flex" role="search">
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ Auth::user()->name }}
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">{{ Auth::user()->email }}</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button type="submit" class="btn btn-link text-decoration-none text-white">Logout</button>
+                    </form>
+                </li>
+            </ul>
+            </li>
+            @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+            </li>
+            @endauth
+        </ul>
+        <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+            <button class="btn btn-outline-light" type="submit">Search</button>
+        </form>
         </div>
-      </div>
+    </div>
     </nav>
 
-    <div class="container my-2">
-      @yield('content')
+    {{-- Main Content --}}
+    <div class="container my-4">
+        @yield('content')
     </div>
 
-    <div class="bg-success py-2 text-white text-center">
-      Copyright &copy; 2025 Developed by Yori Adi Atma
+    {{-- Footer --}}
+    <div class="bg-success text-white text-center py-2">
+        Copyright &copy; 2025 - Manajemen Informatika 2A
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
   </body>
 </html>
